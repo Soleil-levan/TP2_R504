151f8f542dc1b507522640b05e5991bb0190a810
Soleil-levan <sinadiop2004@gmail.com>
2024-10-27 16:04:57 +0100

    Modification de la méthode affiche pour accepter un parammètre n et adaptation des tests

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 
-def affiche():
+def affiche(n):
 	return "".join(
 	"FrisBee" if i % 15 == 0 else "Fizz" 
 	if i % 3 == 0 else "Buzz" 
-	for i in range(1, 101)
+	for i in range(1, n+1)
 		)
-print(affiche())
+if __name__ == "__main__":
+	print(affiche(15))
 
--- tests/test_fizzbuzz.py
+++ tests/test_fizzbuzz.py
@@ -1,7 +1,27 @@
+#!/usr/bin/env python
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from fizzbuzz import affiche 
 
-def test_affiche():
-	result = affiche()
-	assert "Fizz" in result, "Fizz devrait app"
-	assert "Buzz" in result, "Buzz devraot app"
-	assert "FrisBee" in result, "FrisBee deavrit app"
+def test_affiche_n():
+	result = affiche(15)
+	expected = "12Fizz4BuzzFizz78FizzBuzz11Fizz1314FrisBee"
+	assert result == expected, f"Expected {expected}, but got {result}"
+
+def test_affiche_n_limiter():
+	result = affiche(5)
+	expected = "12Fizz4Buzz"
+	assert result == expected, f"Expected {expected}, but got {result}"
+
+	result = affiche(1)
+	expected = "1"
+	assert result == expected, f"Expected {expected}, but got {result}"
+
+if __name__ == " __main__":
+	test_affiche_n()
+	test_affiche_n_limiter()
+print("All tests passed")
+
+
