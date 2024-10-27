9c196f128860418506f96b8eb6fbbeaf30e5532b
Soleil-levan <sinadiop2004@gmail.com>
2024-10-27 14:11:44 +0100

    Ajout de la méthode affiche et des tests initiaux pour affichage de 1 à 100 avec règles FizzBuzz

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,16 +1,10 @@
 #!/usr/bin/env python
 
 def affiche():
-	elements = []
-	for i in range (1, 101):
-		if i % 15 == 0:
-			elements.append("frisBee")
-		elif i % 3 ==0:
-			elements.append("Fizz")	
-		elif i % 5 == 0:
-		 elements.append("Buzz")
-		else:
-		 elements.append(str(i))
-	return "".join(elements)		
-
+	return "".join(
+	"FrisBee" if i % 15 == 0 else "Fizz" 
+	if i % 3 == 0 else "Buzz" 
+	for i in range(1, 101)
+		)
 print(affiche())
+
