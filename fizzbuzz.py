80d7d9b1e62ecea0714ab861f1aedf57f4391b0f
Soleil-levan <sinadiop2004@gmail.com>
2024-10-29 17:01:06 +0100

    Ajout de la fonction affiche avec deux paramètres n1 et n2 et tests associés

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,13 +1,23 @@
 #!/usr/bin/env python
 
-def affiche(n1, n2 + 1):
-	elements = []
-	for i in ra
-	return "".join(
-	"FrisBee" if i % 15 == 0 else "Fizz" 
-	if i % 3 == 0 else "Buzz" 
-	for i in range(1, n+1)
-		)
-if __name__ == "__main__":
-	print(affiche(15))
+def affiche(n1, n2):
+	if not isinstance(n1, int) or not isinstance(n2, int):
+		raise ValueError("Les paramètres doivent être des entiers.")
+	
+	if n1 > n2:
+		 raise ValueError("n1 ne doit pas être supérieur à n2.")
 
+	elements = []
+	for i in range(n1, n2 + 1):
+		if i % 15 == 0:
+			elements.append("FrisBee")
+		elif i % 3 == 0:
+			elements.append("Fizz")
+		elif i % 5 == 0:
+			elements.append("Buzz")
+		else:
+			elements.append(str(i))
+	return "".join(elements)
+
+print(affiche(5, 10))
+print(affiche(10, 16))
